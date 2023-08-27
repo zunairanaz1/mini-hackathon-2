600a04d3a1cd58a67f158fda52a149bf25bbf32c
zunairanaz1 <122044560+zunairanaz1@users.noreply.github.com>
2023-08-27 20:17:03 +0500

    Add files via upload

---
+++ user/js/firebase.mjs
@@ -0,0 +1,11 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCyTIhWcQX8dl80WkNukZ-rRccV5tdXbGE",
+    authDomain: "hackathon-454e5.firebaseapp.com",
+    projectId: "hackathon-454e5",
+    storageBucket: "hackathon-454e5.appspot.com",
+    messagingSenderId: "673898755005",
+    appId: "1:673898755005:web:765b945d08c3d812f358d4",
+    measurementId: "G-P801KDCQT4"
+  };
